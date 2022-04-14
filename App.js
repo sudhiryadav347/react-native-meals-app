974421ad70e8ca83a7823a529d23e89cc39d4296
Sudhir Yadav <sudhiryadav347@gmail.com>
2022-04-14 23:42:14 +0530

    Getting started with React Navigation package

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import Categoriesscreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.rootView}>
-      <StatusBar style="light" />
-      <Categoriesscreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <StatusBar style="dark" />
+      <Stack.Navigator>
+        <Stack.Screen name="Meal Categories" component={Categoriesscreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
