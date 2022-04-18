7e7802e3928eb150c5b797a01fd595b0e76818bf
Sudhir Yadav <sudhiryadav347@gmail.com>
2022-04-18 12:18:31 +0530

    Managing App-wide State with Context

--- store/context/favorites-context.js
+++ store/context/favorites-context.js
@@ -1,12 +1,33 @@
 import { createContext } from 'react';
 
 const FavoritesContext = createContext({
-    ids: [],
-    addFavorite: (id) => {},
-    removeFavorite: (id) => {}
+  ids: [],
+  addFavorite: (id) => {},
+  removeFavorite: (id) => {},
 });
 
-export default FavoritesContextProvider = ({children}) => { 
-    return <FavoritesContext.Provider>{children}</FavoritesContext.Provider>
-}
+export default FavoritesContextProvider = ({ children }) => {
+  const [favoriteMealIds, setfavoriteMealIds] = useState([]);
 
+  const addFavorite = (id) => {
+    setfavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  };
+
+  const removeFavorite = (id) => {
+    setfavoriteMealIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  };
+
+  const value = {
+    ids: favoriteMealIds,
+    addfavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
