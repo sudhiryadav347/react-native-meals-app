eddcf66cf4da4f1e73983504d4a8a658e2026bfb
Sudhir Yadav <sudhiryadav347@gmail.com>
2022-04-19 15:23:20 +0530

    Using Redux useState in Components to display data.

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,22 +1,26 @@
 import { View, Text, StyleSheet } from 'react-native';
-import { useContext } from 'react';
+import { useSelector } from 'react-redux';
+// import { useContext } from 'react';
 import { MEALS } from '../data/dummy-data';
-import { FavoritesContext } from '../store/context/favorites-context';
+// import { FavoritesContext } from '../store/context/favorites-context';
 import MealsList from '../components/MealsList/MealsList';
 
 const FavoritesScreen = () => {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  // const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
   const favoriteMeals = MEALS.filter((meal) =>
-    favoriteMealsCtx.ids.includes(meal.id)
+    // favoriteMealsCtx.ids.includes(meal.id)
+    favoriteMealIds.includes(meal.id)
   );
   // console.log('favoriteMeals', favoriteMealsCtx.ids);
 
-  if (favoriteMeals.length === 0){
-    return <View style={styles.rootContainer}>
-      <Text style={styles.text}>You have no favorite items.</Text>
-    </View>
-  }
-  else{ 
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite items.</Text>
+      </View>
+    );
+  } else {
     return <MealsList data={favoriteMeals} />;
   }
 };
@@ -33,5 +37,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 18,
     color: '#8C2E26',
-  }
+  },
 });
